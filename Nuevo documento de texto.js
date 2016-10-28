b2a1b38526b11b25ce57f4f995b48f2987737fab
guidoivanespadasmadera <5812a60024344@mailbox92.biz>
2016-10-27 20:36:48 -0500

    Agregar Línea
    se agregó una nueva línea

--- Nuevo documento de texto.js
+++ Nuevo documento de texto.js
@@ -2,5 +2,6 @@ public class Hola{
 
      public static void main(String[] args){
                    System.out.println(Hola Mundo");
+                   System.out.println(Hola Mundo2");
    }
            }
